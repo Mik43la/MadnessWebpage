4b447344f3c25a6efea2c231ec5ec8b9f692465e
camigrandy <camgany@gmail.com>
2023-07-02 12:13:44 -0400

    fix: logo

--- madness-web-page/src/user/pages/ArtistsPage.tsx
+++ madness-web-page/src/user/pages/ArtistsPage.tsx
@@ -7,9 +7,7 @@ function ArtistsPage() {
         <>
         <NavBar></NavBar>
         <div className="p-6 mx-10 flex flex-start">
-            <p>
-                Events
-            </p>
+            <h1 className="text-4xl font-bold">Artistas</h1>
         </div>
         <div className="pb-10 pt-5 pl-10 pr-10 mx-auto ">
             <ArtistsSection></ArtistsSection>
